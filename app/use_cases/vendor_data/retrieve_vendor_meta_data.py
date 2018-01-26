030315513dba59107bb37686747ca34c1642e07e
Edmond O' Flynn <oflynned@tcd.ie>
2018-01-26 15:18:28 +0000

    updated vendor schema

--- app/use_cases/vendor_data/retrieve_vendor_meta_data.py
+++ app/use_cases/vendor_data/retrieve_vendor_meta_data.py
@@ -5,15 +5,16 @@ class RetrieveVendorMetaData:
             {
                 "vendor": "GlassByte Barbers",
                 "meta": {
-                    "target_gender": "male",
                     "logo": "http://www.glassbyte.com/images/logo.png",
                     "location": {
-                        "lat": 53.359755,
-                        "lng": -6.261555
+                        "address": "Leeson St. Upper, Ballsbridge, Dublin 4",
+                        "lat": 53.329213,
+                        "lng": -6.248808
                     },
                     "tags": [
-                        "dry", "student"
-                    ]
+                        "student", "crew cut"
+                    ],
+                    "ig_username": "earwax_man"
                 },
                 "opening_hours": {
                     "monday": {"opening": "9:00", "closing": "19:00"},
@@ -30,18 +31,98 @@ class RetrieveVendorMetaData:
                     "Beard Trim": 7,
                     "Student Deal": 8,
                     "Children": 7
-                },
-                "barbers": [
-                    {
-                        "forename": "Edmond",
-                        "surname": "Ó Floinn",
-                        "ig_username": "earwax_man"
+                }
+            },
+            {
+                "vendor": "Best Barbers",
+                "meta": {
+                    "logo": "http://www.glassbyte.com/images/logo.png",
+                    "location": {
+                        "address": "Dromheath Dr., Mulhuddart, Dublin 15",
+                        "lat": 53.404912,
+                        "lng": -6.390206
                     },
-                    {
-                        "forename": "Alexandru",
-                        "surname": "Sulea",
-                        "ig_username": "alexsulea"
-                    }
-                ]
+                    "tags": [
+                        "razor", "hot towel", "design"
+                    ],
+                    "ig_username": "alexsulea"
+                },
+                "opening_hours": {
+                    "monday": {"opening": "9:00", "closing": "19:00"},
+                    "tuesday": {"opening": "9:00", "closing": "19:00"},
+                    "wednesday": {"opening": "9:00", "closing": "19:00"},
+                    "thursday": {"opening": "9:00", "closing": "20:00"},
+                    "friday": {"opening": "9:00", "closing": "20:00"},
+                    "saturday": {"opening": "9:00", "closing": "18:00"},
+                    "sunday": {"opening": "9:00", "closing": "18:00"},
+                },
+                "prices": {
+                    "Dry Cut": 10,
+                    "Wet Cut": 15,
+                    "Beard Trim": 6,
+                    "Shave": 10.50
+                }
+            },
+            {
+                "vendor": "Fades & Blades",
+                "meta": {
+                    "logo": "http://www.glassbyte.com/images/logo.png",
+                    "location": {
+                        "address": "North Circular Rd., Phibsborough, Dublin 7",
+                        "lat": 53.359741,
+                        "lng": -6.261692
+                    },
+                    "tags": [
+                        "turkish", "shave"
+                    ],
+                    "ig_username": "earwax_man"
+                },
+                "opening_hours": {
+                    "monday": {"opening": "9:00", "closing": "19:00"},
+                    "tuesday": {"opening": "9:00", "closing": "19:00"},
+                    "wednesday": {"opening": "9:00", "closing": "19:00"},
+                    "thursday": {"opening": "9:00", "closing": "20:00"},
+                    "friday": {"opening": "9:00", "closing": "20:00"},
+                    "saturday": {"opening": "9:00", "closing": "18:00"},
+                    "sunday": {"opening": "9:00", "closing": "18:00"},
+                },
+                "prices": {
+                    "Wet Cut": 15,
+                    "Dry Cut": 10,
+                    "Beard Trim": 7,
+                    "Student Deal": 8,
+                    "Children": 7
+                }
+            },
+            {
+                "vendor": "Grafton Barbers",
+                "meta": {
+                    "logo": "http://www.glassbyte.com/images/logo.png",
+                    "location": {
+                        "address": "Grafton St., Dublin 2",
+                        "lat": 53.340175,
+                        "lng": -6.260629
+                    },
+                    "tags": [
+                        "fade", "taper", "kids"
+                    ],
+                    "ig_username": "earwax_man"
+                },
+                "opening_hours": {
+                    "monday": {"opening": "9:00", "closing": "19:00"},
+                    "tuesday": {"opening": "9:00", "closing": "19:00"},
+                    "wednesday": {"opening": "9:00", "closing": "19:00"},
+                    "thursday": {"opening": "9:00", "closing": "20:00"},
+                    "friday": {"opening": "9:00", "closing": "20:00"},
+                    "saturday": {"opening": "9:00", "closing": "18:00"},
+                    "sunday": {"opening": "9:00", "closing": "18:00"},
+                },
+                "prices": {
+                    "Wet Cut": 15,
+                    "Dry Cut": 10,
+                    "Beard Trim": 7,
+                    "Student Deal": 8,
+                    "Children": 7
+                }
             }
         ]
