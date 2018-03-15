cf494f06fb58d98a1ec87726a5ffc16f9422f095
Edmond O' Flynn <oflynned@tcd.ie>
2018-03-15 20:56:40 +0000

    updated __init__ and app to have template and static folders

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,24 @@
-from flask import Flask, Response
+import os
+
+from flask import Flask, Response, render_template
 from flask_pymongo import PyMongo
 
-from app.api.vendor import vendor_endpoint
 from app.api.content import content_endpoint
+from app.api.vendor import vendor_endpoint
 
-app = Flask(__name__)
+frontend_dir = os.path.abspath("templates/")
+static_dir = os.path.abspath("static/")
+app = Flask(__name__, template_folder=frontend_dir, static_folder=static_dir)
+
 app.register_blueprint(vendor_endpoint, url_prefix="/api/v1/vendor")
 app.register_blueprint(content_endpoint, url_prefix="/api/v1/content")
 
 app.config["MONGO_URI"] = "mongodb://localhost:27017/hair"
+
+
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("index.html")
 
 
 @app.route("/ping", methods=["GET"])
--- app/app.py
+++ app/app.py
@@ -3,6 +3,6 @@ from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
-app.config["MONGO_URI"] = "mongodb://localhost:27017/hair"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/trym"
 
 mongo = PyMongo(app)
